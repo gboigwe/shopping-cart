ca0541ab7a71aa87f2da2eb443b5c8fa51b1e659
Gbolahan Akande <geakande@gmail.com>
2025-05-17 12:27:30 +0100

    Updated cart context

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Products from './pages/Products';
 // import Checkout from './pages/Checkout';
+import { CartProvider } from './context/CartContext';
+import CartSidebar from './components/CartSidebar';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Products />} />
-        {/* <Route path="/checkout" element={<Checkout />} /> */}
-      </Routes>
-    </Router>
+    <CartProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Products />} />
+          {/* <Route path="/checkout" element={<Checkout />} /> */}
+        </Routes>
+        <CartSidebar />
+      </Router>
+    </CartProvider>
   );
 }
 
